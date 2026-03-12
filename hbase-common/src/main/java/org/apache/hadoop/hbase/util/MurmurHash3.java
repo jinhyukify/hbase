bbcadd2b4943a99dd16b000b3b88e0412660cd67
jinhyukify <jinhyukify@gmail.com>
2026-03-12 15:14:14 +0900

    murmur3 example

--- hbase-common/src/main/java/org/apache/hadoop/hbase/util/MurmurHash3.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/util/MurmurHash3.java
@@ -19,6 +19,8 @@ package org.apache.hadoop.hbase.util;
 
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.yetus.audience.InterfaceStability;
+import com.dynatrace.hash4j.hashing.Hasher32;
+import com.dynatrace.hash4j.hashing.Hashing;
 
 /**
  * This is a very fast, non-cryptographic hash suitable for general hash-based lookup. See
@@ -40,56 +42,8 @@ public class MurmurHash3 extends Hash {
   /** Returns the MurmurHash3_x86_32 hash. */
   @edu.umd.cs.findbugs.annotations.SuppressWarnings("SF")
   @Override
-  public <T> int hash(HashKey<T> hashKey, int initval) {
-    final int c1 = 0xcc9e2d51;
-    final int c2 = 0x1b873593;
-    int length = hashKey.length();
-    int h1 = initval;
-    int roundedEnd = (length & 0xfffffffc); // round down to 4 byte block
-
-    for (int i = 0; i < roundedEnd; i += 4) {
-      // little endian load order
-      int k1 = (hashKey.get(i) & 0xff) | ((hashKey.get(i + 1) & 0xff) << 8)
-        | ((hashKey.get(i + 2) & 0xff) << 16) | (hashKey.get(i + 3) << 24);
-      k1 *= c1;
-      k1 = (k1 << 15) | (k1 >>> 17); // ROTL32(k1,15);
-      k1 *= c2;
-
-      h1 ^= k1;
-      h1 = (h1 << 13) | (h1 >>> 19); // ROTL32(h1,13);
-      h1 = h1 * 5 + 0xe6546b64;
-    }
-
-    // tail
-    int k1 = 0;
-
-    switch (length & 0x03) {
-      case 3:
-        k1 = (hashKey.get(roundedEnd + 2) & 0xff) << 16;
-        // FindBugs SF_SWITCH_FALLTHROUGH
-      case 2:
-        k1 |= (hashKey.get(roundedEnd + 1) & 0xff) << 8;
-        // FindBugs SF_SWITCH_FALLTHROUGH
-      case 1:
-        k1 |= (hashKey.get(roundedEnd) & 0xff);
-        k1 *= c1;
-        k1 = (k1 << 15) | (k1 >>> 17); // ROTL32(k1,15);
-        k1 *= c2;
-        h1 ^= k1;
-      default:
-        // fall out
-    }
-
-    // finalization
-    h1 ^= length;
-
-    // fmix(h1);
-    h1 ^= h1 >>> 16;
-    h1 *= 0x85ebca6b;
-    h1 ^= h1 >>> 13;
-    h1 *= 0xc2b2ae35;
-    h1 ^= h1 >>> 16;
-
-    return h1;
+  public <T> int hash(HashKey<T> hashKey, int seed) {
+    Hasher32 hasher = seed == 0 ? Hashing.murmur3_32() : Hashing.murmur3_32(seed);
+    return hasher.hashBytesToInt(hashKey, 0, hashKey.length(), HashKeyByteAccess.INSTANCE);
   }
 }
